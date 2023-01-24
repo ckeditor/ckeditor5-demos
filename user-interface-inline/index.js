c47fb7de1d03ea15ea60e80477096d16d80c3d76
illia sheremetov <i.sheremetov@cksource.com>
2023-01-24 16:48:26 +0100

    inline editor is finished

--- user-interface-inline/index.js
+++ user-interface-inline/index.js
@@ -13,6 +13,10 @@ import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+/* You must provide a valid token URL in order to use the CKBox application.
+After registering to CKBox, the fastest way to try out CKBox is to use the development token endpoint:
+https://ckeditor.com/docs/ckbox/latest/guides/configuration/authentication.html#token-endpoint*/
+import CKBox from '@ckeditor/ckeditor5-ckbox/src/ckbox';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
@@ -40,6 +44,10 @@ const defaultConfig = {
 		BlockQuote,
 		CKFinder,
 		CloudServices,
+		/* You must provide a valid token URL in order to use the CKBox application.
+		After registering to CKBox, the fastest way to try out CKBox is to use the development token endpoint:
+		https://ckeditor.com/docs/ckbox/latest/guides/configuration/authentication.html#token-endpoint*/
+		// CKBox,
 		Heading,
 		Image,
 		ImageCaption,
@@ -65,6 +73,10 @@ const defaultConfig = {
 		'bold', 'italic',
 		'|',
 		'link', 'uploadImage', 'insertTable', 'blockQuote',
+		/* You must provide a valid token URL in order to use the CKBox application.
+		After registering to CKBox, the fastest way to try out CKBox is to use the development token endpoint:
+		https://ckeditor.com/docs/ckbox/latest/guides/configuration/authentication.html#token-endpoint*/
+		// 'link', 'uploadImage', 'ckbox' ,'insertTable', 'blockQuote',
 		'|',
 		'bulletedList', 'numberedList',
 		'|',
@@ -129,6 +141,12 @@ const headerConfig = {
 		addTargetToExternalLinks: true,
 		defaultProtocol: 'https://'
 	}
+	/* You must provide a valid token URL in order to use the CKBox application.
+	After registering to CKBox, the fastest way to try out CKBox is to use the development token endpoint:
+	https://ckeditor.com/docs/ckbox/latest/guides/configuration/authentication.html#token-endpoint*/
+	// ckbox: {
+	// 	tokenUrl: 'https://your.token.url'
+	// } );
 };
 
 const inlineElementsIds = [ 'inline-header', 'inline-main', 'inline-footer-first', 'inline-footer-second' ];
