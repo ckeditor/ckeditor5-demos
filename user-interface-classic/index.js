3e2726af26d28969a088983f9f0f1004f5218153
illia sheremetov <i.sheremetov@cksource.com>
2023-01-24 16:26:21 +0100

    classic editor finished

--- user-interface-classic/index.js
+++ user-interface-classic/index.js
@@ -14,6 +14,10 @@ import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+/* You must provide a valid token URL in order to use the CKBox application.
+After registering to CKBox, the fastest way to try out CKBox is to use the development token endpoint:
+https://ckeditor.com/docs/ckbox/latest/guides/configuration/authentication.html#token-endpoint*/
+// import CKBox from '@ckeditor/ckeditor5-ckbox/src/ckbox';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
@@ -42,6 +46,10 @@ ClassicEditor.create( document.querySelector( '#cke5-user-interface-classic-demo
 		Bold,
 		CKFinder,
 		CloudServices,
+		/* You must provide a valid token URL in order to use the CKBox application.
+		After registering to CKBox, the fastest way to try out CKBox is to use the development token endpoint:
+		https://ckeditor.com/docs/ckbox/latest/guides/configuration/authentication.html#token-endpoint*/
+		// CKBox,
 		Essentials,
 		Heading,
 		Image,
@@ -75,6 +83,10 @@ ClassicEditor.create( document.querySelector( '#cke5-user-interface-classic-demo
 		'bold', 'italic', 'underline',
 		'|',
 		'link', 'uploadImage', 'insertTable', 'blockQuote', 'mediaEmbed',
+		/* You must provide a valid token URL in order to use the CKBox application.
+		After registering to CKBox, the fastest way to try out CKBox is to use the development token endpoint:
+		https://ckeditor.com/docs/ckbox/latest/guides/configuration/authentication.html#token-endpoint*/
+		// 'link', 'uploadImage', 'insertTable', 'blockQuote', 'ckbox' ,'mediaEmbed',
 		'|',
 		'bulletedList', 'numberedList',
 		'|',
@@ -127,6 +139,12 @@ ClassicEditor.create( document.querySelector( '#cke5-user-interface-classic-demo
 			'mergeTableCells'
 		]
 	}
+	/* You must provide a valid token URL in order to use the CKBox application.
+	After registering to CKBox, the fastest way to try out CKBox is to use the development token endpoint:
+	https://ckeditor.com/docs/ckbox/latest/guides/configuration/authentication.html#token-endpoint*/
+	// ckbox: {
+	// 	tokenUrl: 'https://your.token.url'
+	// } );
 } )
 	.then( editor => {
 		window.editor = editor;
