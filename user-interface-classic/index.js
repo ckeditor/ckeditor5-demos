5ea28504225754f500a06468be2daa20e1169143
Marek Lewandowski <code@mlewandowski.com>
2023-01-27 16:53:07 +0100

    Added a missing comma.

--- user-interface-classic/index.js
+++ user-interface-classic/index.js
@@ -87,7 +87,7 @@ ClassicEditor.create( document.querySelector( '#cke5-user-interface-classic-demo
 		/* You must provide a valid token URL in order to use the CKBox application.
 		After registering to CKBox, the fastest way to try out CKBox is to use the development token endpoint:
 		https://ckeditor.com/docs/ckbox/latest/guides/configuration/authentication.html#token-endpoint */
-		// 'ckbox'
+		// 'ckbox',
 		'insertTable',
 		'blockQuote',
 		'mediaEmbed',
