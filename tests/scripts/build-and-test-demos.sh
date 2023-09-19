eacb29003f1961cc83ad990f602452839ab9e85c
Łukasz Gudel <58658048+LukaszGudel@users.noreply.github.com>
2023-09-19 10:35:33 +0200

    Migrate travis flow to CircleCI (#30)
    Tests: Migrate travis flow to CircleCI.

--- tests/scripts/build-and-test-demos.sh
+++ tests/scripts/build-and-test-demos.sh
@@ -3,6 +3,8 @@
 # Set error variable
 ERROR=0
 
+DEMOS_PATH=`pwd`
+
 # Prepare the environment: install dependencies and build samples
 for DIR in $DEMOS_PATH/* ; do
     if [[ -d "$DIR" && ! "$DIR" = "$DEMOS_PATH/tests" ]]; then
@@ -14,11 +16,11 @@ for DIR in $DEMOS_PATH/* ; do
         yarn > /dev/null 2>&1
         # Build demo
         echo "Building demo: $DEMO_NAME"
-        webpack --mode development > /dev/null
+        npx webpack --mode development > /dev/null
 
         if [ ! $? -eq 0 ]; then
-                echo "Building failed: $DEMO_NAME"
-                ERROR=1
+            echo "Building failed: $DEMO_NAME"
+            ERROR=1
         fi
     fi
 done
@@ -29,20 +31,6 @@ echo "Samples building completed."
 echo "Starting up the server."
 http-server $DEMOS_PATH -p 9001 -s &
 
-# Check if the server is alive
-for i in $(seq 1 60); do
-    echo "Waiting for the server..."
-    curl -I http://localhost:9001 --max-time 5 > /dev/null 2>&1
-
-    if [ $? = 0 ]; then
-        break
-    fi
-
-    sleep 1
-done
-
-PID=$!
-
 # Start tests
 cd $DEMOS_PATH/tests
 yarn run cy:test-demos
@@ -51,8 +39,6 @@ if [ ! $? -eq 0 ]; then
         echo "Some tests failed."
         ERROR=1
 fi
-
-kill $PID
 
 if [ $ERROR -eq 1 ]; then
     exit 1
