4020c7088e86495906306d98f3e2710ff040b5ac
f1ames <k.krzton@cksource.com>
2024-05-28 11:42:47 +0200

    Migrate 'user-interface-classic' demo to NIM.

---
+++ user-interface-classic/index.ts
@@ -0,0 +1,206 @@
+/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+// CKEditor Commercial Features require a license key to work properly.
+// * You can get a trial license key: https://orders.ckeditor.com/trial/premium-features.
+// * Or you can comment out (disable) the plugins imported from the "ckeditor5-premium-features" package.
+const LICENSE_KEY = '';
+
+if (!LICENSE_KEY) {
+	alert(
+		'CKEditor Commercial Features included in this demo require a license key.\n' +
+		'Check the index.ts file for more information.'
+	);
+}
+
+// CKBox plugin requires a valid token URL in order to use the CKBox application.
+// After registering to CKBox, the fastest way to try out CKBox is to use the development token endpoint:
+// https://ckeditor.com/docs/ckbox/latest/guides/configuration/authentication.html#token-endpoint
+const CKBOX_TOKEN_URL = '';
+
+import {
+	ClassicEditor,
+	Autoformat,
+	Bold,
+	Italic,
+	Underline,
+	BlockQuote,
+	Base64UploadAdapter,
+	CKFinder,
+	CKFinderUploadAdapter,
+	CloudServices,
+	CKBox,
+	Essentials,
+	Heading,
+	Image,
+	ImageCaption,
+	ImageResize,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	PictureEditing,
+	Indent,
+	IndentBlock,
+	Link,
+	List,
+	MediaEmbed,
+	Mention,
+	Paragraph,
+	PasteFromOffice,
+	Table,
+	TableColumnResize,
+	TableToolbar,
+	TextTransformation,
+} from 'ckeditor5';
+
+import {
+	SlashCommand
+} from 'ckeditor5-premium-features';
+
+import 'ckeditor5/index.css';
+import 'ckeditor5-premium-features/index.css';
+
+ClassicEditor.create(
+	document.querySelector('#cke5-user-interface-classic-demo') as HTMLElement,
+	{
+		plugins: [
+			Autoformat,
+			BlockQuote,
+			Bold,
+			CKFinder,
+			CKFinderUploadAdapter,
+			CloudServices,
+			...(CKBOX_TOKEN_URL ? [CKBox] : []),
+			Essentials,
+			Heading,
+			Image,
+			ImageCaption,
+			ImageResize,
+			ImageStyle,
+			ImageToolbar,
+			ImageUpload,
+			Base64UploadAdapter,
+			Indent,
+			IndentBlock,
+			Italic,
+			Link,
+			List,
+			MediaEmbed,
+			Mention,
+			Paragraph,
+			PasteFromOffice,
+			PictureEditing,
+			Table,
+			TableColumnResize,
+			TableToolbar,
+			TextTransformation,
+			Underline,
+			...(LICENSE_KEY ? [SlashCommand] : []),
+		],
+		licenseKey: LICENSE_KEY,
+		toolbar: [
+			'undo',
+			'redo',
+			'|',
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'underline',
+			'|',
+			'link',
+			'uploadImage',
+			'ckbox',
+			'insertTable',
+			'blockQuote',
+			'mediaEmbed',
+			'|',
+			'bulletedList',
+			'numberedList',
+			'|',
+			'outdent',
+			'indent',
+		],
+		heading: {
+			options: [
+				{
+					model: 'paragraph',
+					title: 'Paragraph',
+					class: 'ck-heading_paragraph',
+				},
+				{
+					model: 'heading1',
+					view: 'h1',
+					title: 'Heading 1',
+					class: 'ck-heading_heading1',
+				},
+				{
+					model: 'heading2',
+					view: 'h2',
+					title: 'Heading 2',
+					class: 'ck-heading_heading2',
+				},
+				{
+					model: 'heading3',
+					view: 'h3',
+					title: 'Heading 3',
+					class: 'ck-heading_heading3',
+				},
+				{
+					model: 'heading4',
+					view: 'h4',
+					title: 'Heading 4',
+					class: 'ck-heading_heading4',
+				},
+			],
+		},
+		image: {
+			resizeOptions: [
+				{
+					name: 'resizeImage:original',
+					label: 'Default image width',
+					value: null,
+				},
+				{
+					name: 'resizeImage:50',
+					label: '50% page width',
+					value: '50',
+				},
+				{
+					name: 'resizeImage:75',
+					label: '75% page width',
+					value: '75',
+				},
+			],
+			toolbar: [
+				'imageTextAlternative',
+				'toggleImageCaption',
+				'|',
+				'imageStyle:inline',
+				'imageStyle:wrapText',
+				'imageStyle:breakText',
+				'imageStyle:side',
+				'|',
+				'resizeImage',
+			],
+		},
+		link: {
+			addTargetToExternalLinks: true,
+			defaultProtocol: 'https://',
+		},
+		table: {
+			contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
+		},
+		ckbox: {
+			tokenUrl: CKBOX_TOKEN_URL,
+		},
+	}
+)
+.then((editor) => {
+	(window as any).editor = editor;
+})
+.catch((error) => {
+	console.error(error.stack);
+});
