8157041e91ad127d197e53d2e5a1a25d86a6ff79
f1ames <k.krzton@cksource.com>
2024-05-28 09:54:48 +0200

    Migrate 'user-interface-balloon-block' demo to NIM.

---
+++ user-interface-balloon-block/index.ts
@@ -0,0 +1,168 @@
+/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+// CKEditor Commercial Features require a license key to work properly.
+// * You can get a trial license key: https://orders.ckeditor.com/trial/premium-features.
+// * Or you can comment out (disable) the plugins imported from the "ckeditor5-premium-features" package.
+const LICENSE_KEY = '';
+
+if (!LICENSE_KEY) {
+	alert(
+		'CKEditor Commercial Features included in this demo require a license key.\n' +
+		'Check the index.ts file for more information.'
+	);
+}
+
+import {
+	BalloonEditor,
+	Autoformat,
+	Bold,
+	Italic,
+	BlockQuote,
+	Base64UploadAdapter,
+	CKFinder,
+	CKFinderUploadAdapter,
+	CloudServices,
+	Essentials,
+	Heading,
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	PictureEditing,
+	Indent,
+	IndentBlock,
+	Link,
+	List,
+	MediaEmbed,
+	Mention,
+	Paragraph,
+	PasteFromOffice,
+	Table,
+	TableToolbar,
+	TextTransformation,
+	BlockToolbar,
+} from 'ckeditor5';
+
+import {
+	SlashCommand
+} from 'ckeditor5-premium-features';
+
+import 'ckeditor5/index.css';
+import 'ckeditor5-premium-features/index.css';
+
+BalloonEditor.create(
+	document.querySelector('#cke5-user-interface-balloon-block-demo') as HTMLElement,
+	{
+		plugins: [
+			Autoformat,
+			BlockQuote,
+			BlockToolbar,
+			Bold,
+			CKFinder,
+			CKFinderUploadAdapter,
+			CloudServices,
+			Essentials,
+			Heading,
+			Image,
+			ImageCaption,
+			ImageStyle,
+			ImageToolbar,
+			ImageUpload,
+			Base64UploadAdapter,
+			Indent,
+			IndentBlock,
+			Italic,
+			Link,
+			List,
+			MediaEmbed,
+			Mention,
+			Paragraph,
+			PasteFromOffice,
+			PictureEditing,
+			Table,
+			TableToolbar,
+			TextTransformation,
+			...(LICENSE_KEY ? [SlashCommand] : []),
+		],
+		licenseKey: LICENSE_KEY,
+		blockToolbar: [
+			'undo',
+			'redo',
+			'|',
+			'heading',
+			'|',
+			'uploadImage',
+			'insertTable',
+			'blockQuote',
+			'mediaEmbed',
+			'|',
+			'bulletedList',
+			'numberedList',
+			'outdent',
+			'indent',
+		],
+		toolbar: {
+			items: ['bold', 'italic', 'link'],
+		},
+		heading: {
+			options: [
+				{
+					model: 'paragraph',
+					title: 'Paragraph',
+					class: 'ck-heading_paragraph',
+				},
+				{
+					model: 'heading1',
+					view: 'h1',
+					title: 'Heading 1',
+					class: 'ck-heading_heading1',
+				},
+				{
+					model: 'heading2',
+					view: 'h2',
+					title: 'Heading 2',
+					class: 'ck-heading_heading2',
+				},
+				{
+					model: 'heading3',
+					view: 'h3',
+					title: 'Heading 3',
+					class: 'ck-heading_heading3',
+				},
+				{
+					model: 'heading4',
+					view: 'h4',
+					title: 'Heading 4',
+					class: 'ck-heading_heading4',
+				},
+			],
+		},
+		image: {
+			toolbar: [
+				'imageStyle:inline',
+				'imageStyle:block',
+				'imageStyle:side',
+				'|',
+				'toggleImageCaption',
+				'imageTextAlternative',
+			],
+		},
+		link: {
+			addTargetToExternalLinks: true,
+			defaultProtocol: 'https://',
+		},
+		table: {
+			contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
+		},
+	}
+)
+.then((editor) => {
+	(window as any).editor = editor;
+})
+.catch((error) => {
+	console.error(error.stack);
+});
